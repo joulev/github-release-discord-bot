32443a43cfddedc87f0f5b1362d0b551fe74232c
Vu Van Dung <me@joulev.dev>
2023-09-04 08:27:02 +0700

    restructure
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,6 @@ import { Octokit } from "@octokit/rest";
 import { Client, Events, GatewayIntentBits } from "discord.js";
 import { env } from "./env.js";
 
-const botClient = new Client({ intents: [GatewayIntentBits.Guilds] });
-botClient.once(Events.ClientReady, main);
-botClient.login(env.DISCORD_TOKEN);
-
-const octokit = new Octokit({ auth: env.GITHUB_TOKEN });
-
 interface GitHubRelease {
   name: string | null;
   time: Date;
@@ -20,56 +14,70 @@ class LastUpdatedStore {
   public constructor() {
     this.lastUpdated = new Date();
   }
-  public get(): Date {
-    return this.lastUpdated;
+  public releaseIsNewer(release: GitHubRelease): boolean {
+    return release.time > this.lastUpdated;
   }
   public update(): void {
     this.lastUpdated.setTime(Date.now());
   }
 }
 
-async function getNewReleases(lastUpdatedStore: LastUpdatedStore): Promise<GitHubRelease[]> {
-  const lastUpdated = lastUpdatedStore.get();
-  const releases = await octokit.repos
-    .listReleases({ owner: env.REPO_OWNER, repo: env.REPO_NAME })
-    .then(res =>
-      res.data.map(release => ({
-        name: release.name,
-        time: release.published_at ? new Date(release.published_at) : null,
-        url: release.html_url,
-        body: release.body,
-      })),
-    );
-  return releases.filter((release): release is GitHubRelease => {
-    if (release.time === null) return false;
-    return release.time > lastUpdated;
-  });
-}
+class ReleaseChecker {
+  private readonly client: Client<true>;
+  private readonly octokit = new Octokit({ auth: env.GITHUB_TOKEN });
+  private readonly lastUpdatedStore = new LastUpdatedStore();
 
-async function postNewRelease(client: Client<true>, release: GitHubRelease) {
-  const releaseChannel = client.channels.cache.get(env.RELEASE_CHANNEL_ID);
-  if (!releaseChannel || !releaseChannel.isTextBased()) {
-    console.error("Release channel is invalid");
-    return;
+  options = {
+    revalidate: 1000 * 60, // 1 minute
+  };
+
+  public constructor(client: Client<true>) {
+    this.client = client;
   }
-  await releaseChannel.send(`**${release.name}**\n${release.url}\n${release.body}`);
-}
 
-async function check(client: Client<true>, lastUpdatedStore: LastUpdatedStore) {
-  const releases = await getNewReleases(lastUpdatedStore);
-  for (const release of releases.sort((a, b) => a.time.getTime() - b.time.getTime())) {
-    // eslint-disable-next-line no-await-in-loop -- We want to ensure the order is correct
-    await postNewRelease(client, release);
+  async getNewReleases(): Promise<GitHubRelease[]> {
+    const releases = await this.octokit.repos
+      .listReleases({ owner: env.REPO_OWNER, repo: env.REPO_NAME })
+      .then(res =>
+        res.data.map(release => ({
+          name: release.name,
+          time: release.published_at ? new Date(release.published_at) : null,
+          url: release.html_url,
+          body: release.body,
+        })),
+      );
+    return releases
+      .filter((release): release is GitHubRelease => release.time !== null)
+      .filter(release => this.lastUpdatedStore.releaseIsNewer(release));
   }
-  lastUpdatedStore.update();
+
+  async postNewRelease(release: GitHubRelease) {
+    const releaseChannel = this.client.channels.cache.get(env.RELEASE_CHANNEL_ID);
+    if (!releaseChannel || !releaseChannel.isTextBased()) {
+      console.error("Release channel is invalid");
+      return;
+    }
+    await releaseChannel.send(`**${release.name}**\n${release.url}\n${release.body}`);
+  }
+
+  async check() {
+    const releases = await this.getNewReleases();
+    for (const release of releases.sort((a, b) => a.time.getTime() - b.time.getTime())) {
+      // eslint-disable-next-line no-await-in-loop -- We want to ensure the order is correct
+      await this.postNewRelease(release);
+    }
+    this.lastUpdatedStore.update();
+  }
+
+  public async run() {
+    await this.check();
+    setInterval(() => void this.check(), this.options.revalidate);
+  }
 }
 
-async function main(client: Client<true>) {
-  console.log(`Ready! Logged in as ${client.user.tag}`);
-
-  const lastUpdatedStore = new LastUpdatedStore();
-  await check(client, lastUpdatedStore);
-
-  const ONE_MINUTE = 1000 * 60;
-  setInterval(() => void check(client, lastUpdatedStore), ONE_MINUTE);
-}
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.once(Events.ClientReady, c => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+  new ReleaseChecker(c).run();
+});
+client.login(env.DISCORD_TOKEN);
